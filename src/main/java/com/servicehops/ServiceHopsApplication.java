028278241fd0e6c15aeeaf8c95a0737687fb9cb4
Harshal patil <91362856+Harshal141@users.noreply.github.com>
2026-01-31 13:13:42 +0530

    init repo

---
+++ src/main/java/com/servicehops/ServiceHopsApplication.java
@@ -0,0 +1,13 @@
+package com.servicehops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servicehops/ServiceHopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicehops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
